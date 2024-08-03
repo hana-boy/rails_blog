44fe6b1ee211f1f698a61adc000bb327b5f779d6
Kaihanafusa <85861384+Kaihanafusa@users.noreply.github.com>
2024-08-03 11:27:13 +0900

    create crud functions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,37 @@
 class ArticlesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     articles = Article.all
     render status: 200, json: { articles: articles }
+  end
+
+  def show
+    article = Article.find(params[:id])
+    render status: 200, json: article
+  end
+
+  def create
+    request_body = params.permit(:user_id, :title, :content, :url)
+    article = Article.create(request_body)
+    render status: 200, json: article.id
+  end
+
+  def update
+    request_body = params.permit(:user_id, :title, :content, :url)
+    article = Article.find(params[:id])
+    article.user_id = params[:user_id]
+    article.title = params[:title]
+    article.content = params[:content]
+    article.url = params[:url]
+    article.save
+
+    render status: 200, json: article.id
+  end
+
+  def destroy
+    Article.delete(params[:id])
+
+    render status: 200, json: { result: true }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   resources :users, only: :index
-  resources :articles, only: :index
+  resources :articles
 
   # Defines the root path route ("/")
   # root "posts#index"
